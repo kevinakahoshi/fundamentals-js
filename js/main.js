85435629c7e307cb7489d8ad41eaede18874f14f
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-02-05 17:58:58 +0000

    Added new files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { closures } from './lessons/closures.js';
+
 document.onreadystatechange = () => {
   if (document.readyState === 'interactive') {
     initializeApp();
@@ -5,9 +7,10 @@ document.onreadystatechange = () => {
 };
 
 function initializeApp() {
-  const contentArea = document.getElementsByClassName('main');
+  const contentArea = document.getElementsByClassName('content');
   const heading = document.createElement('h1');
   const headingText = 'This is a test';
   heading.innerText = headingText;
   contentArea[0].appendChild(heading);
+  console.log(lessons);
 }
