a7dca0351e2d5dc1c6f8234866833e5abcaf4d45
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-03-06 17:31:53 +0000

    Added smooth transitions

--- js/lessons/functions.js
+++ js/lessons/functions.js
@@ -1,3 +1,6 @@
-export const functions = () => {
-  return 'The functions function has run successfully';
+export const functions = contentArea => {
+  const headingElement = document.createElement('h1');
+  headingElement.innerText = 'The functions function has run successfully';
+  headingElement.classList.add('mount');
+  contentArea.appendChild(headingElement);
 }
